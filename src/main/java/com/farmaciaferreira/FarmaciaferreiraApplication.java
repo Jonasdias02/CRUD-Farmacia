ad5b809b29908bf142b7412cba36888a51154e1b
Jonas <jonasdanova@gmail.com>
2023-10-19 09:34:36 -0300

    Iniciando o Projeto dia 19/10/23

---
+++ src/main/java/com/farmaciaferreira/FarmaciaferreiraApplication.java
@@ -0,0 +1,13 @@
+package com.farmaciaferreira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaferreiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaferreiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmaciaferreira/FarmaciaferreiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciaferreira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaferreiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
